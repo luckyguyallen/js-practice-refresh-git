d84e755eee292c1c262797425d604fbff7eae81a
mastermalone <mastermalone@mastermalone-popeye.(none)>
2015-03-22 20:45:29 -0700

    For loop practice

---
+++ scripts/practice.js
@@ -0,0 +1,18 @@
+"use strict";
+(function(){
+	function myFunction(){
+		var goat, 
+		nameList = ["mike", "erin", "russell", "Isaiah"], 
+		rst = document.getElementById("results");
+		
+		for( var i = 0; i < nameList.length; i++){
+			console.log("refresh", nameList[i], "jjjjjj");
+			goat = document.createElement("LI");
+			var txt = document.createTextNode(nameList[i]);
+			goat.appendChild(txt);
+			rst.appendChild(goat);
+		}
+		console.log("working");
+	}
+	myFunction();
+}());
